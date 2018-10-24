ac48d396b145d7db223e96948ce0e3d9cab1ef18
Petr F <petr.fedchenko@gmail.com>
2018-10-24 17:55:56 +0300

    q

--- js/news.js
+++ js/news.js
@@ -1,11 +1,31 @@
 let apiKey = '24b4f3580d754a929e72f26298be2481';
 let url = 'https://newsapi.org/v2/top-headlines?country=ua&category=science&apiKey='
 
+let news;
+let data = '';
+
 fetch(`${url}${apiKey}`)
   .then((response) => {
     return response.json();
    })
   .then((data) => {
-      console.log(2, data);
+      news = data.articles;
   })
   .catch( alert );
+
+$(document).ready(() => {
+    for(let i = 0; i < 5; i++) {
+        news[i].content ? addNews(news[i]) : i--;
+    }
+
+    $(".content").html(data);
+});
+
+function addNews(news) {
+    data +=
+        `<div class="item-news">
+            <div class="title">${news.title}</div>
+            <div class=""img>${news.urlImg}</div>
+            <div class=""text>${news.content}</div>
+        </div>`;
+}
