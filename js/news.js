81e2108e1e341750bc7424450b0faf9db53d3c37
Petr F <petr.fedchenko@gmail.com>
2018-10-25 20:17:53 +0300

    New file

--- js/news.js
+++ js/news.js
@@ -1,31 +1,39 @@
 let apiKey = '24b4f3580d754a929e72f26298be2481';
 let url = 'https://newsapi.org/v2/top-headlines?country=ua&category=science&apiKey='
 
-let news;
-let data = '';
+let dataHtml = '';
 
-$(document).ready(function() {
-    fetch(`${url}${apiKey}`)
-    .then((response) => {
-    return response.json();
-    })
-    .then((data) => {
-        news = data.articles;
-    })
-    .catch( alert );
+$(document).ready(function () {
+    fetch(`${url}${apiKey}`) // Сюда мы подставлем ссылку
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            contentData(data.articles);
+        })
+        .catch(alert);
 
-    for(let i = 0; i < 5; i++) {
-        news[i].content ? addNews(news[i]) : i--;
-    }
-
-    $(".content").html(data);
 });
 
+function contentData(data) {
+    for (let i = 0; i < data.length; i++) {
+        data[i].content ? addNews(data[i]) : null;
+    }
+
+    $(".data").html(dataHtml);
+};
+
 function addNews(news) {
-    data +=
-        `<div class="item-news">
+    dataHtml +=
+        `<div class="content">
             <div class="title">${news.title}</div>
-            <div class=""img>${news.urlImg}</div>
-            <div class=""text>${news.content}</div>
+            <div class="news">
+                <div class="text">
+                    <div class="image">
+                        <img src="${news.urlToImage}" alt="Картинка">
+                        ${news.content}
+                    </div>
+                </div>
+            </div>
         </div>`;
-}
+};
